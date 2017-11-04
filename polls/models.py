af6fde654bd2d438a5d514a06dfeef026fb298f9
james sewell <jamesjosephsewell@gmail.com>
2017-11-04 17:28:46 -0500

    added a method to questions, also tested out the api a bit more in the shell and determined that everything is working as expected

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
